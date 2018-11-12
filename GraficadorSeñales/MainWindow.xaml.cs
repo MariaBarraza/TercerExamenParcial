d1ae16e24f18adea574508ee06b7827c9cfecefd
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-12 08:18:54 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+
+    {
+        //Guardar la amplitud de la señal mas grande para que ninguna se salga de rango
+        double amplitudMaxima = 1;
+
+        //instancia de la clase señal senoidal
+        Señal señal;
+        Señal segundaSeñal;
+        Señal señalResultado;
+
+        public MainWindow()
+        {
+            InitializeComponent(); //cualquier codigo que se ponga debajo de esta se ejecuta al iniciar el programa
+
+
+
+        }
+
+        private void btnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            //para obtener el valor del text box se usa la propiedad .Text
+
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+
+            switch (cbTipoSeñal.SelectedIndex)
+            {
+                //señal senoidal
+                case 0:
+                    //el primer hijo del panel configuracion es la configuracion senoidal y es de otro tipo (ui collection)asi que se hace un casting y asi se puede acceder a sus propiedades, ademas como txtamplitud es tipo texto se usa parse
+                    //nota: los hijos son los elementos de los contenedores
+                    double amplitud = double.Parse(((ConfiguracionSeñalSenoidal)panelConfiguracion.Children[0]).txtAmplitud.Text);
+                    double fase = double.Parse(((ConfiguracionSeñalSenoidal)panelConfiguracion.Children[0]).txtFase.Text);
+                    double frecuencia = double.Parse(((ConfiguracionSeñalSenoidal)panelConfiguracion.Children[0]).txtFrecuencia.Text);
+
+                    señal = new SeñalSenoidal(amplitud, fase, frecuencia);
+                    break;
+                //rampa
+                case 1:
+                    señal = new SeñalRampa();
+                    break;
+                case 2:
+                    double alpha = double.Parse(((ConfiguracionSeñalExponencial)panelConfiguracion.Children[0]).txtAlpha.Text);
+                    señal = new SeñalExponencial(alpha);
+                    break;
+                case 3:
+                    señal = new SeñalRectangular();
+                    break;
+                default:
+                    señal = null;
+                    break;
+            }
+
+            //Señal 2
+            switch (cbTipoSeñal_SegundaSeñal.SelectedIndex)
+            {
+                //señal senoidal
+                case 0:
+                    //el primer hijo del panel configuracion es la configuracion senoidal y es de otro tipo (ui collection)asi que se hace un casting y asi se puede acceder a sus propiedades, ademas como txtamplitud es tipo texto se usa parse
+                    //nota: los hijos son los elementos de los contenedores
+                    double amplitud = double.Parse(((ConfiguracionSeñalSenoidal)panelConfiguracion_SegundaSeñal.Children[0]).txtAmplitud.Text);
+                    double fase = double.Parse(((ConfiguracionSeñalSenoidal)panelConfiguracion_SegundaSeñal.Children[0]).txtFase.Text);
+                    double frecuencia = double.Parse(((ConfiguracionSeñalSenoidal)panelConfiguracion_SegundaSeñal.Children[0]).txtFrecuencia.Text);
+
+                    segundaSeñal = new SeñalSenoidal(amplitud, fase, frecuencia);
+                    break;
+                //rampa
+                case 1:
+                    segundaSeñal = new SeñalRampa();
+                    break;
+                case 2:
+                    double alpha = double.Parse(((ConfiguracionSeñalExponencial)panelConfiguracion_SegundaSeñal.Children[0]).txtAlpha.Text);
+                    segundaSeñal = new SeñalExponencial(alpha);
+                    break;
+                case 3:
+                    segundaSeñal = new SeñalRectangular();
+                    break;
+                default:
+                    segundaSeñal = null;
+                    break;
+            }
+
+            //se establecen los valores para la funcion
+            señal.TiempoInicial = tiempoInicial;
+            señal.TiempoFinal = tiempoFinal;
+            señal.FrecuenciaMuestreo = frecuenciaMuestreo;
+
+            segundaSeñal.TiempoInicial = tiempoInicial;
+            segundaSeñal.TiempoFinal = tiempoFinal;
+            segundaSeñal.FrecuenciaMuestreo = frecuenciaMuestreo;
+
+
+            //se ejecuta la funcion
+            señal.construirSeñalDigital();
+            segundaSeñal.construirSeñalDigital();
+
+            if ((bool)cbAmplitud.IsChecked)
+            {
+                //Escalar
+                double factorEscala = double.Parse(txtFactorEscalaAmplitud.Text);
+                señal.escalar(factorEscala);
+            }
+
+            if ((bool)cbDesplazar.IsChecked)
+            {
+                //Desplazar
+                double factorDesplazar = double.Parse(txtFactorDesplazamiento.Text);
+                señal.desplazar(factorDesplazar);
+            }
+
+            if ((bool)cbTruncar_SegundaSeñal.IsChecked)
+            {
+                //Truncar
+                double factorTruncar = double.Parse(txtFactorTruncar.Text);
+                señal.truncar(factorTruncar);
+            }
+
+            //Segunda Señal
+            if ((bool)cbAmplitud_SegundaSeñal.IsChecked)
+            {
+                //Escalar
+                double factorEscala = double.Parse(txtFactorEscalaAmplitud_SegundaSeñal.Text);
+                segundaSeñal.escalar(factorEscala);
+            }
+
+            if ((bool)cbDesplazar_SegundaSeñal.IsChecked)
+            {
+                //Desplazar
+                double factorDesplazar = double.Parse(txtFactorDesplazamiento_SegundaSeñal.Text);
+                segundaSeñal.desplazar(factorDesplazar);
+            }
+
+            if ((bool)cbTruncar_SegundaSeñal.IsChecked)
+            {
+                //Truncar
+                double factorTruncar = double.Parse(txtFactorTruncar_SegundaSeñal.Text);
+                segundaSeñal.truncar(factorTruncar);
+            }
+
+            //Aqui se toma la amplitud maxima de la señal para poder hacer que se tome el valor de la mayor para que se adapte el panel
+            señal.actualizarAmplitudMaxima();
+            segundaSeñal.actualizarAmplitudMaxima();
+            amplitudMaxima = señal.AmplitudMaxima;
+            
+            if(segundaSeñal.AmplitudMaxima> amplitudMaxima)
+            {
+                amplitudMaxima = segundaSeñal.AmplitudMaxima;
+            }
+
+            //cambia los valores de la etiqueta
+            //La F es que da el formato para redondear a 2 decimales, la funcion ToString puede recibir un parametro que es el que va a decidir en que formato va a estar,existen varios parametros
+            lblAmplitudMaximaPositivaY.Text = amplitudMaxima.ToString("F");
+            lblAmplitudMaximaNegativaY.Text = "-" + amplitudMaxima.ToString("F");
+
+            // limpiar la grafica
+            plnGrafica.Points.Clear();
+            plnGraficaDos.Points.Clear();
+
+            //hacerlo si la señal no es nula
+           if(señal != null)
+            {
+                //recorrer una coleccion o arreglo
+                //muestra toma el valor de señal.muestra en cada recorrido del ciclo
+                foreach (Muestra muestra in señal.Muestras)
+                {
+                    //se evalua la señal, luego se ajusta y de ahi se agrega el punto
+                    plnGrafica.Points.Add(new Point((muestra.X - tiempoInicial) * scrContenedor.Width, (muestra.Y / amplitudMaxima * ((scrContenedor.Height / 2) - 30) * -1) + (scrContenedor.Height / 2)));
+                }
+
+            }
+            // hacerlo si la Segunda señal no es nula
+            if (segundaSeñal != null)
+            {
+                //recorrer una coleccion o arreglo
+                //muestra toma el valor de señal.muestra en cada recorrido del ciclo
+                foreach (Muestra muestra in segundaSeñal.Muestras)
+                {
+                    //se evalua la señal, luego se ajusta y de ahi se agrega el punto
+                    plnGraficaDos.Points.Add(new Point((muestra.X - tiempoInicial) * scrContenedor.Width, (muestra.Y / amplitudMaxima * ((scrContenedor.Height / 2) - 30) * -1) + (scrContenedor.Height / 2)));
+                }
+
+            }
+
+            //Graficando el eje de X
+            plnEjeX.Points.Clear();
+            //Punto de inicio.
+            plnEjeX.Points.Add(new Point(0, (scrContenedor.Height / 2)));
+            //Punto de fin.
+            plnEjeX.Points.Add(new Point((tiempoFinal - tiempoInicial) * scrContenedor.Width, (scrContenedor.Height / 2)));
+
+            //Graficando el eje de Y
+            plnEjeY.Points.Clear();
+            //Punto de inicio.
+            plnEjeY.Points.Add(new Point(0 - tiempoInicial * scrContenedor.Width, scrContenedor.Height));
+            //Punto de fin.
+            plnEjeY.Points.Add(new Point(0 - tiempoInicial * scrContenedor.Width, scrContenedor.Height * -1));
+
+        }
+
+        private void btnGraficarRampa_Click(object sender, RoutedEventArgs e)
+        {
+            //tomar valores
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
+
+            //instancia
+            SeñalRampa señal = new SeñalRampa();
+
+            //calcular periodomuestreo
+            double periodoMuestreo = 1 / frecuenciaMuestreo;
+
+            //limpiar puntos
+            plnGrafica.Points.Clear();
+
+            //ciclo para conseguir las muestras
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
+            {
+                double valorMuestra = señal.evaluar(i);
+                señal.Muestras.Add(new Muestra(i, valorMuestra));
+
+                //se van añadiendo las muestras a las listas
+                señal.Muestras.Add(new Muestra(i, valorMuestra)); 
+
+            }
+
+            //ciclo para recorrer muestras
+            foreach (Muestra muestra in señal.Muestras)
+            {
+                //se evalua la señal, luego se ajusta y de ahi se agrega el punto
+                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, (muestra.Y * ((scrContenedor.Height / 2) - 30) * -1) + (scrContenedor.Height / 2)));
+            }
+
+        }
+
+        private void cbTipoSeñal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+                //quita los elementos que puede tener el panel
+                panelConfiguracion.Children.Clear();
+
+                switch (cbTipoSeñal.SelectedIndex)
+                {
+                    case 0: //senoidal
+                        //se añaden los elementos al panel (los hijos)
+                        panelConfiguracion.Children.Add(
+                          new ConfiguracionSeñalSenoidal() 
+                            );
+
+                        break;
+                    case 1: //Rampa
+                        break;
+                    case 2:
+                    panelConfiguracion.Children.Add(
+                  new ConfiguracionSeñalExponencial()
+                    );
+                    break;
+                default:
+                        break;
+                } 
+        }
+
+        private void cbAmplitud_Checked(object sender, RoutedEventArgs e)
+        {
+            txtFactorEscalaAmplitud.IsEnabled = true;
+           
+        }
+
+        private void cbAmplitud_UnChecked(object sender, RoutedEventArgs e)
+        {
+            txtFactorEscalaAmplitud.IsEnabled = false;
+        }
+
+        
+
+        private void cbDesplazar_Checked(object sender, RoutedEventArgs e)
+        {
+            txtFactorDesplazamiento.IsEnabled = true;
+
+        }
+
+        private void cbDesplazar_UnChecked(object sender, RoutedEventArgs e)
+        {
+            txtFactorDesplazamiento.IsEnabled = false;
+        }
+
+
+        private void cbTruncar_Checked(object sender, RoutedEventArgs e)
+        {
+            txtFactorTruncar.IsEnabled = true;
+
+        }
+
+        private void cbTruncar_UnChecked(object sender, RoutedEventArgs e)
+        {
+            txtFactorTruncar.IsEnabled = false;
+        }
+
+        private void cbTipoSeñal_SegundaSeñal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //quita los elementos que puede tener el panel
+            panelConfiguracion_SegundaSeñal.Children.Clear();
+
+            switch (cbTipoSeñal_SegundaSeñal.SelectedIndex)
+            {
+                case 0: //senoidal
+                        //se añaden los elementos al panel (los hijos)
+                    panelConfiguracion_SegundaSeñal.Children.Add(
+                      new ConfiguracionSeñalSenoidal()
+                        );
+
+                    break;
+                case 1: //Rampa
+                    break;
+                case 2:
+                    panelConfiguracion_SegundaSeñal.Children.Add(
+                  new ConfiguracionSeñalExponencial()
+                    );
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnRealizarOperacion_Click(object sender, RoutedEventArgs e)
+        {
+            señalResultado = null;
+            switch (cbTipoOperacion.SelectedIndex)
+            {
+                case 0: //Suma
+                    //los metodos estaticos no necesitan una instancia
+                    señalResultado = Señal.sumar(señal, segundaSeñal);
+                    break;
+                case 1: //multiplicacion
+                    señalResultado = Señal.multiplicar(señal, segundaSeñal);
+                    break;
+                case 2: //convolucion
+                    señalResultado = Señal.convolucionar(señal, segundaSeñal);
+                    break;
+                case 3:
+                    señalResultado = Señal.correlacionar(señal, segundaSeñal);
+                    break;
+                default:
+                    break;
+            }
+
+            //se actualiza la amplitud maxima del resultado 
+            señalResultado.actualizarAmplitudMaxima();
+
+            plnGraficaResultado.Points.Clear();
+
+            //cambia los valores de la etiqueta
+            //La F es que da el formato para redondear a 2 decimales, la funcion ToString puede recibir un parametro que es el que va a decidir en que formato va a estar,existen varios parametros
+            lblAmplitudMaximaPositivaY_Resultado.Text = señalResultado.AmplitudMaxima.ToString("F");
+            lblAmplitudMaximaNegativaY_Resultado.Text = "-" + señalResultado.AmplitudMaxima.ToString("F");
+
+            //hacerlo si la señal no es nula
+            if (señalResultado != null)
+            {
+                //recorrer una coleccion o arreglo
+                //muestra toma el valor de señal.muestra en cada recorrido del ciclo
+                foreach (Muestra muestra in señalResultado.Muestras)
+                {
+                    //se evalua la señal, luego se ajusta y de ahi se agrega el punto
+                    plnGraficaResultado.Points.Add(new Point((muestra.X - señalResultado.TiempoInicial) * scrContenedor_Resultado.Width, (muestra.Y / señalResultado.AmplitudMaxima * ((scrContenedor_Resultado.Height / 2) - 30) * -1) + (scrContenedor_Resultado.Height / 2)));
+                }
+
+            }
+   
+            //Graficando el eje de X
+            plnEjeXResultado.Points.Clear();
+            //Punto de inicio.
+            plnEjeXResultado.Points.Add(new Point(0, (scrContenedor_Resultado.Height / 2)));
+            //Punto de fin.
+            plnEjeXResultado.Points.Add(new Point((señalResultado.TiempoFinal - señalResultado.TiempoInicial) * scrContenedor_Resultado.Width, (scrContenedor_Resultado.Height / 2)));
+
+            //Graficando el eje de Y
+            plnEjeYResultado.Points.Clear();
+            //Punto de inicio.
+            plnEjeYResultado.Points.Add(new Point((0 - señalResultado.TiempoInicial) * scrContenedor_Resultado.Width,(((scrContenedor_Resultado.Height/2.0)-30)*-1) + (scrContenedor_Resultado.Height/2)));
+            //Punto de fin.
+            plnEjeYResultado.Points.Add(new Point((0 - señalResultado.TiempoInicial) * scrContenedor_Resultado.Width, (-1 * ((scrContenedor_Resultado.Height / 2.0)-30) * -1) + (scrContenedor_Resultado.Height/2)));
+
+        }
+    }
+}
+
---
+++ GraficadorSeñales/Señal.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales 
+{
+    abstract class Señal
+    {
+        //Es un arreglo dinamico
+        public List<Muestra> Muestras { get; set; }
+
+        public double AmplitudMaxima { get; set; }
+        public double TiempoInicial { get; set; }
+        public double TiempoFinal { get; set; }
+        public double FrecuenciaMuestreo { get; set; }
+        
+
+        public abstract double evaluar(double tiempo);
+
+        public void construirSeñalDigital()
+        {
+            //calcular el periodo 
+            double periodoMuestreo = 1 / FrecuenciaMuestreo; //esto se llama sacar el inverso
+
+            //Se construye la señal digital, se delimita el tiempo con el for
+            for (double i = TiempoInicial; i <= TiempoFinal; i += periodoMuestreo)
+            {
+                double valorMuestra = evaluar(i);
+
+                //se calcula el numero mas alto que puede tomar la señal
+                if (Math.Abs(valorMuestra) > AmplitudMaxima)
+                {
+                   AmplitudMaxima = Math.Abs(valorMuestra);
+                }
+
+                //se van añadiendo las muestras a las listas
+                Muestras.Add(new Muestra(i, valorMuestra));
+            }
+
+        }
+
+        public void escalar(double factor)
+        {
+            //por cara muestra se va a realizar esto
+           foreach(Muestra muestra in Muestras)
+            {
+                //se multiplica por Y para escalar, no por X para conservar el instante de tiempo
+                muestra.Y *= factor;
+            }
+        }
+
+        public void actualizarAmplitudMaxima()
+        {
+            AmplitudMaxima = 0;
+            
+            foreach(Muestra muestra in Muestras)
+            {
+                if(Math.Abs(muestra.Y) > AmplitudMaxima)
+                {
+                    AmplitudMaxima = Math.Abs(muestra.Y);
+                }
+            }
+        }
+
+        public void desplazar(double factor)
+        {
+            //por cada muestra se va a realizar esto
+            foreach (Muestra muestra in Muestras)
+            {
+                //se suma en Y para desplazar, no por X para conservar el instante de tiempo
+                muestra.Y += factor;
+            }
+        }
+
+        public void truncar(double umbral)
+        {
+            //por cada muestra se va a realizar esto
+            foreach (Muestra muestra in Muestras)
+            {
+                //se suma en Y para desplazar, no por X para conservar el instante de tiempo
+                if(muestra.Y>umbral)
+                {
+                    muestra.Y = umbral;
+                }
+                else
+                    if(muestra.Y < -umbral)
+                {
+                    muestra.Y = -umbral;
+                }
+            }
+        }
+        //se hace abstracta para no necesitar crear instancias
+        public static Señal sumar(Señal sumando1, Señal sumando2)
+        {
+
+            //construimos la señal resultado
+            SeñalPersonalizada resultado = new SeñalPersonalizada();
+
+            //tomamos los valores de tiempo y muestreo
+            resultado.TiempoInicial = sumando1.TiempoInicial;
+            resultado.TiempoFinal = sumando1.TiempoFinal;
+            resultado.FrecuenciaMuestreo = sumando1.FrecuenciaMuestreo;
+
+            //sumamos muestra por muestra
+            //recorremos 1 lista de muestras y a la 2 señal accedemos por un indice
+            int indice = 0;
+            foreach(Muestra muestra in sumando1.Muestras)
+            {
+                Muestra muestraResultado = new Muestra();
+                muestraResultado.X = muestra.X;
+                muestraResultado.Y = muestra.Y + sumando2.Muestras[indice].Y;
+                indice++;
+                resultado.Muestras.Add(muestraResultado);
+            }
+
+            return resultado;
+        }
+
+        public static Señal multiplicar(Señal factor1,Señal factor2)
+        {
+            SeñalPersonalizada resultado = new SeñalPersonalizada();
+            resultado.TiempoInicial = factor1.TiempoInicial;
+            resultado.TiempoFinal = factor1.TiempoFinal;
+            resultado.FrecuenciaMuestreo = factor1.FrecuenciaMuestreo;
+
+            int indice = 0;
+            foreach (Muestra muestra in factor1.Muestras)
+            {
+                Muestra muestraResultado = new Muestra();
+                muestraResultado.X = muestra.X;
+                muestraResultado.Y = muestra.Y * factor2.Muestras[indice].Y;
+                indice++;
+                resultado.Muestras.Add(muestraResultado);
+            }
+
+            return resultado;
+        }
+
+       
+        public static Señal convolucionar(Señal operando1,Señal operando2)
+        {
+            SeñalPersonalizada resultado = new SeñalPersonalizada();
+
+            //asi se deteminan los limites de una convolucion, es la suma de los limites interiores y la suma de los limites superiores
+            resultado.TiempoInicial = operando1.TiempoInicial + operando2.TiempoInicial;
+            resultado.TiempoFinal = operando1.TiempoFinal + operando2.TiempoFinal;
+
+            resultado.FrecuenciaMuestreo = operando1.FrecuenciaMuestreo;
+
+            double periodoMuestreo = 1 / resultado.FrecuenciaMuestreo;
+
+            double duracionSeñal = resultado.TiempoFinal - resultado.TiempoInicial;
+
+            double cantidadMuestrasResultado = duracionSeñal * resultado.FrecuenciaMuestreo;
+
+            double instanteActual = resultado.TiempoInicial;
+            for (int n = 0; n<cantidadMuestrasResultado;n++)
+            {
+                double valorMuestraY = 0;
+                for(int k=0; k < operando2.Muestras.Count;k++)
+                {
+                    // el .count es una propiedad que permite contar el numero de valores que tiene una lista
+                    if((n-k) >= 0 && (n-k) < operando2.Muestras.Count)
+                    {
+                        valorMuestraY += operando1.Muestras[k].Y * operando2.Muestras[n - k].Y;
+                    } 
+                }
+
+                valorMuestraY /= resultado.FrecuenciaMuestreo;
+
+                Muestra muestra = new Muestra(instanteActual, valorMuestraY);
+                resultado.Muestras.Add(muestra);
+                instanteActual += periodoMuestreo;
+            }
+
+                return resultado;
+        }
+
+        public static Señal correlacionar(Señal operando1, Señal operando2)
+        {
+            SeñalPersonalizada resultado = new SeñalPersonalizada();
+
+            //asi se deteminan los limites de una convolucion, es la suma de los limites interiores y la suma de los limites superiores
+            resultado.TiempoInicial = operando1.TiempoInicial - (operando2.TiempoFinal - operando2.TiempoInicial);
+            resultado.TiempoFinal = operando1.TiempoFinal;
+
+            resultado.FrecuenciaMuestreo = operando1.FrecuenciaMuestreo;
+
+            double periodoMuestreo = 1 / resultado.FrecuenciaMuestreo;
+
+            double duracionSeñal = resultado.TiempoFinal - resultado.TiempoInicial;
+
+            double cantidadMuestrasResultado = duracionSeñal * resultado.FrecuenciaMuestreo;
+
+            double instanteActual = resultado.TiempoInicial;
+            for (int n = 0; n < cantidadMuestrasResultado; n++)
+            {
+                double valorMuestraY = 0;
+                for (int k = 0; k < operando2.Muestras.Count; k++)
+                {
+                    // el .count es una propiedad que permite contar el numero de valores que tiene una lista
+                    if ((n + k) >= 0 && (n + k) < operando2.Muestras.Count)
+                    {
+                        valorMuestraY += operando1.Muestras[k].Y * operando2.Muestras[n + k].Y;
+                    }
+                }
+
+                valorMuestraY /= resultado.FrecuenciaMuestreo;
+
+                Muestra muestra = new Muestra(instanteActual, valorMuestraY);
+                resultado.Muestras.Add(muestra);
+                instanteActual += periodoMuestreo;
+            }
+
+
+
+            return resultado;
+        }
+
+    }
+}
